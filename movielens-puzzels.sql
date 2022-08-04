388eed233f14dbe8d2fa518ecd52a2894a58d13e
Stephanie Ashdown <80210351+Stephanie-Ash@users.noreply.github.com>
2022-08-04 16:50:48 +0100

    Correct incorrect answers

--- movielens-puzzels.sql
+++ movielens-puzzels.sql
@@ -3,15 +3,23 @@ USE movielens;
 
 -- List the titles and release dates of movies released between 1983-1993 in reverse chronological order
 SELECT title, release_date FROM movies
-WHERE release_date BETWEEN '1983-01-01' AND '1993-12-31'
+WHERE release_date BETWEEN '1983-01-01' AND '1993-01-01'
 ORDER BY release_date DESC;
 
 -- Without using LIMIT , list the titles of the movies with the lowest average rating
-SELECT m.title, r.rating FROM movies m
-INNER JOIN ratings r ON m.id=r.movie_id
-WHERE r.rating=(
-	SELECT MIN(rating) FROM ratings
-);
+SELECT title, AVG(rating) AS average_rating
+FROM movies
+JOIN ratings ON movies.id = ratings.movie_id
+GROUP BY movies.title
+HAVING average_rating = (
+	SELECT MIN(avg_rating)
+	FROM (
+		SELECT AVG(rating) AS avg_rating
+		FROM ratings
+		GROUP BY movie_id
+	) AS table1
+)
+ORDER BY average_rating;
 
 -- List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings
 SELECT DISTINCT m.title, g.`name`, u.age, u.gender, r.rating, o.`name` FROM genres g
